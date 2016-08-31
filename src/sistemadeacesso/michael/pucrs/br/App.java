dc3ec64518491aa45579ed665defc5b3066e34fa
pedrocasarao <pedrohmprado@gmail.com>
2016-08-30 22:29:45 -0300

    implementando o metodo  priorizaçaão na classe caixa e colocando o menu dentro do APP

--- src/sistemadeacesso/michael/pucrs/br/App.java
+++ src/sistemadeacesso/michael/pucrs/br/App.java
@@ -6,19 +6,49 @@ public class App {
 	
 	public static void main(String []args) 
 	{
+		int opcao=0;
 		ArrayList<Cliente> caixa = new ArrayList<>();
-		System.out.println("Digite sua idade: ");
-		Scanner lerIdade  = new 	Scanner(System.in);
-		int idade = lerIdade.nextInt();
-		System.out.println("sua idade é " +idade+", Digite o seu nome: ");
-		Scanner lerNome = new Scanner(System.in);
-		String nome = lerNome.nextLine();
 		
-		System.out.println("Bom dia "+nome +", sua idade é: "+idade);
-		Cliente c = new Cliente(nome,idade);
-		caixa.add(c);
+		switch (opcao) {
+		case 1:
+			//adcionar cliente
+			
+			System.out.println("Digite sua idade: ");
+			Scanner lerIdade  = new 	Scanner(System.in);
+			int idade = lerIdade.nextInt();
+			System.out.println("sua idade é " +idade+", Digite o seu nome: ");
+			Scanner lerNome = new Scanner(System.in);
+			String nome = lerNome.nextLine();
+			Cliente e = new Cliente(nome, idade);
+			caixa.add(e);
+			break;
+		case 2:
+			// remover cliente
+			String nome="";
+			System.out.println("digite o nome do cliente que voce atendeu:"+nome);
+			
+			for(int i=0; i<caixa.size();i++){	
+				if(caixa.get(i).getNome() = nome)
+				caixa.remove(e);
+			}
+			
+			break;	
+			
+
+		case 3:
+			// verificar lista de espera
+			caixa.size();
+
+			System.out.println("tamanho da fila é  :"+caixa.size());
+			break;
+		default:
+		
+			break;
+		}
 		
 		
-		System.out.println("tamanho da fila é  :"+caixa.size());
+	
+		
+		
 	}
 }
--- src/sistemadeacesso/michael/pucrs/br/Caixa.java
+++ src/sistemadeacesso/michael/pucrs/br/Caixa.java
@@ -7,12 +7,13 @@ public class Caixa {
 	
 	public void atendimentoPriorizado(ArrayList<Cliente> caixa)
 	{
+		ArrayList<Cliente> aux = new ArrayList<>();
 		for(int i =0 ; i < caixa.size();i++)
 		{
 			for(int j=1;i<caixa.size();i++)
-			if(caixa.get(i).getIdade()> 60 && caixa.get(j).getIdade()< caixa.get(i).getIdade())
+			if(caixa.get(i).getIdade()> 60 && caixa.get(i).getIdade()> caixa.get(j).getIdade())
 			{
-				
+				aux.add(caixa.get(i));
 			}
 				
 		}
